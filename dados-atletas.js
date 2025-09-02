5dc77fa64dd6af2e5430f39c58e985726cec0767
Anderson <andersonfox04@gmail.com>
2025-09-02 20:36:50 -0300

    Update dados-atletas.js

--- dados-atletas.js
+++ dados-atletas.js
@@ -7,51 +7,97 @@ constructor(nome, idade, peso, altura, notas){
     this.notas=notas;
 }
 // calcula a actegoria do atleta
-    // calculaCategoria() {
+     calculaCategoria() {
+        let categoria = this.idade
 
-    //     if (this.idade >= 9 || this.idade <= 11) {
-    //         return "infantil";
-    //     }
+         if (categoria >= 9 && categoria <= 11) {
+            categoria = "infantil";
+         }
 
-    //     if (this.idade >= 12 || this.idade <= 13) {
-    //         return "juvenil";
-    //     }
+         else if (categoria >= 12 && categoria <= 13) {
+             categoria = "juvenil";
+        }
 
-    //     if (this.idade >= 14 || this.idade <= 15) {
-    //         return "intermediário";
-    //     }
+        else if (categoria >= 14 && categoria <= 15) {
+            categoria = "intermediário";
+         }
 
-    //     if (this.idade >= 15 || this.idade <= 16) {
-    //         return "adulto";
-    //     }
+         else if (categoria >= 16 && categoria <= 30){
+             categoria = "adulto";
+         }
 
-    //     else {
-    //         return "sem categoria";
-    //     }
-
-    // }
+         else {
+             categoria = "sem categoria";
+       }
+    
+       console.log(categoria)
+      return categoria
+    }
 //calcula o IMC
-    // calculaIMC(IMC) {
-    // IMC.reduce(function(altura){
-    //      return altura * altura;
-    //     },this.peso);
-    //     console.log(IMC)
-    // }
-//Calcular a media válida
-//     calculaMediaValida(){
+     calculaIMC() {
+     const imc = this.peso / (this.altura * this.altura)
+     console.log(imc)
+     }
 
-//         this.notas.forEach(function(nota){
-//   this.notas = this.notas / this.notas.length
-//     })
-// }
+     
+//Calcular a media válida
+     calculaMediaValida(){
+        let soma = 0
+    this.notas.forEach(function(nota){
+       soma = soma + nota
+       //console.log(nota)
+     })
+     console.log(soma / this.notas.length)
+     return soma / this.notas.length
+     
+ }
 // Obeter o Nome do Atleta
     obtemNomeAtleta(){
-    this.nome.forEach(function() => {
-        
-    });
+   let nomeAtleta= this.nome
+   console.log(nomeAtleta)
+    return nomeAtleta
 }
+// obeter idade do Atleta
+obtemIdadeAtleta(){
+    const idade=this.idade
+    console.log(idade)
+}
+// obeter peso do atleta
+obtemPesoAtleta(){
+let peso=this.peso
+console.log(peso)
+}
+// obter notas
+obtemNotasAtleta(){
+    let notas=this.notas
+    console.log(notas)
+}
+//obeter categorias
+obtemCategoria(){
+let obeterCategoria=this.calculaCategoria()
+}
+
+obtemIMC(){
+    let obeterIMC=this.calculaIMC()
+}
+
+ obtemMediaValida(){
+    let obeterMediaValida=this.calculaMediaValida()
+ }
 
 
 }
 
 const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+// atleta.calculaCategoria()
+// atleta.calculaIMC()
+// atleta.calculaMediaValida()
+atleta.obtemNomeAtleta("nome Atleta",)
+atleta.obtemIdadeAtleta()
+atleta.obtemPesoAtleta()
+atleta.obtemNotasAtleta()
+atleta.obtemCategoria()
+atleta.obtemIMC()
+atleta.obtemMediaValida()
+//console.log(atleta)
